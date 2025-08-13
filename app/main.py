8983548fdc59c5ea2b6275260ca38f8b6bf43a45
mina farajian <farajian.mina70@gmail.com>
2025-08-13 10:37:53 +0330

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+# app/main.py
+
+from flask import Flask
+
+# Create a new Flask application instance
+app = Flask(__name__)
+
+# Define a route for the root URL '/'
+@app.route('/')
+def hello_world():
+    # Return the "Hello, World!" message
+    return 'Hello, World!'
+
+# Run the app if the script is executed directly
+if __name__ == '__main__':
+    # The app will be available on all IP addresses (host='0.0.0.0') and on port 8000
+    app.run(host='0.0.0.0', port=8000)
