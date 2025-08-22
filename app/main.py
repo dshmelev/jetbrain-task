568f68a62f850ae760969a6dba0dde8a1f17ff77
Dmitry Shmelev <Dmitry.Shmelev@jetbrains.com>
2025-08-22 15:51:16 +0200

    add /message

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # app/main.py
 
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,6 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/message')
+def message():
+    return os.getenv("APP_MESSAGE", "APP_MESSAGE is not set")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
