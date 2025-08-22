fd855be11dce1d58a9943a3adb26e414956a8a4e
Dmitry Shmelev <avikez@gmail.com>
2025-08-22 16:37:31 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/version')
+def hello_world():
+    return 'this is version'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
